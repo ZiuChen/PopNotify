3ed7482fbdcc9a561ffb1cd87c04f0a5167999f7
ZiuChen <ZiuChen@outlook.com>
2022-05-20 18:01:47 +0800

    UPDATE

--- src/PopNotify.js
+++ src/PopNotify.js
@@ -12,36 +12,36 @@ class Notify {
     content,
     type,
     fadeTime,
+    position,
     customClass,
     onClick,
     onClose,
     showClose,
     dangerouslyUseHTMLString,
   }) {
-    this._closed = false
-    this._className = "PopNotify" + " " + type
-    this._id = "nid" + new Date().getTime()
-    this._selector = "#" + this._id
-    this._title = title
-    this._content = content
-    this._type = type !== undefined ? type : "info"
-    this._fadeTime = fadeTime !== undefined ? fadeTime : 4500
-    this._customClass = customClass !== undefined ? customClass : undefined
-    this._onClick =
-      onClick !== undefined
-        ? onClick
-        : () => {
-            this.close(this)
-          }
-    this._onClose = onClose !== undefined ? onClose : () => {}
-    this._showClose = showClose !== undefined ? showClose : true
-    this._dangerouslyUseHTMLString =
-      dangerouslyUseHTMLString !== undefined ? dangerouslyUseHTMLString : false
+    this.title = title
+    this.content = content
+    this.type = type || "info"
+    this.fadeTime = fadeTime === undefined ? 4500 : fadeTime
+    this.position = position || "top-left"
+    this.customClass = customClass || undefined
+    this.onClick =
+      onClick ||
+      function () {
+        this.close(this)
+      }
+    this.onClose = onClose || function () {}
+    this.showClose = showClose || true
+    this.dangerouslyUseHTMLString = dangerouslyUseHTMLString || false
+    this.className = `PopNotify ${this.type} ${this.position}`
+    this.id = "nid" + new Date().getTime()
+    this.selector = "#" + this.id
+    this.closed = false
     this.create()
     this.anime("active")
-      .then(() => sleep(this._fadeTime))
+      .then(() => sleep(this.fadeTime))
       .then(() => {
-        return this._fadeTime !== 0 && !this._closed && this.close(this)
+        return this.fadeTime !== 0 && !this.closed && this.close(this)
       })
   }
   create() {
@@ -50,18 +50,16 @@ class Notify {
     let closeBtn = document.createElement("div")
     let firstLine = document.createElement("div")
     let content = document.createElement("div")
-    if (this._dangerouslyUseHTMLString) {
-      title.innerHTML = this._title
-      content.innerHTML = this._content
+    if (this.dangerouslyUseHTMLString) {
+      title.innerHTML = this.title
+      content.innerHTML = this.content
     } else {
-      content.innerText = this._content
-      title.innerText = this._title
+      content.innerText = this.content
+      title.innerText = this.title
     }
-    if (this._customClass) {
-      notify.className = this._className + " " + this._customClass
-    } else {
-      notify.className = this._className
-    }
+    notify.className = this.customClass
+      ? `${this.className} ${this.customClass}`
+      : `${this.className}`
     title.className = "PopNotify_title"
     content.className = "PopNotify_content"
     closeBtn.className = "PopNotify_closeBtn"
@@ -69,90 +67,70 @@ class Notify {
     closeBtn.onclick = () => {
       this.close(this)
     }
-    closeBtn.setAttribute("style", `display: ${this._showClose ? "" : "none"}`)
-    notify.id = this._id
-    notify.onclick = this._onClick
+    closeBtn.setAttribute("style", `display: ${this.showClose ? "" : "none"}`)
+    notify.id = this.id
+    notify.onclick = this.onClick
     firstLine.className = "PopNotify_first-line"
     firstLine.append(title, closeBtn)
     notify.append(firstLine, content)
     document.querySelector("body").append(notify)
   }
   async anime(status) {
-    let el = document.querySelector(this._selector)
+    let el = document.querySelector(this.selector)
     return await sleep(0).then((res) => {
-      el.setAttribute("class", el.className + " " + status)
+      el.className += " " + status
       return res
     })
   }
   close(notify) {
+    if (this.closed) return
     if (!notify) notify = this
-    this._closed = true
+    this.closed = true
     return notify
       .anime("fade")
       .then((res) => {
-        this._onClose()
+        this.onClose()
         return res
       })
       .then(() => sleep(500))
       .then((res) => {
-        document.querySelector(this._selector).remove()
+        document.querySelector(this.selector).remove()
         return res
       })
   }
 }
 
 let PopNotify = {
-  success: (option) => {
-    return new Notify({
-      title: option.title,
-      content: option.content,
-      type: "success",
-      fadeTime: option.fadeTime,
-      customClass: option.customClass,
-      onClick: option.onClick,
-      onClose: option.onClose,
-      showClose: option.showClose,
-      dangerouslyUseHTMLString: option.dangerouslyUseHTMLString,
-    })
+  queue: {
+    "top-left": [],
+    "top-right": [],
+    "bottom-left": [],
+    "bottom-right": [],
   },
-  error: (option) => {
-    return new Notify({
-      title: option.title,
-      content: option.content,
-      type: "error",
-      fadeTime: option.fadeTime,
-      customClass: option.customClass,
-      onClick: option.onClick,
-      onClose: option.onClose,
-      showClose: option.showClose,
-      dangerouslyUseHTMLString: option.dangerouslyUseHTMLString,
-    })
+  notify: function (option) {
+    let notify = new Notify(option)
+    this.queue[notify.position].push(notify)
+    return notify
   },
-  info: (option) => {
-    return new Notify({
-      title: option.title,
-      content: option.content,
-      type: "info",
-      fadeTime: option.fadeTime,
-      customClass: option.customClass,
-      onClick: option.onClick,
-      onClose: option.onClose,
-      showClose: option.showClose,
-      dangerouslyUseHTMLString: option.dangerouslyUseHTMLString,
-    })
+  success: function (option) {
+    option.type = "success"
+    let notify = new Notify(option)
+    return notify
   },
-  warning: (option) => {
-    return new Notify({
-      title: option.title,
-      content: option.content,
-      type: "warning",
-      fadeTime: option.fadeTime,
-      customClass: option.customClass,
-      onClick: option.onClick,
-      onClose: option.onClose,
-      showClose: option.showClose,
-      dangerouslyUseHTMLString: option.dangerouslyUseHTMLString,
-    })
+  error: function (option) {
+    option.type = "error"
+    let notify = new Notify(option)
+    return notify
+  },
+  info: function (option) {
+    option.type = "info"
+    let notify = new Notify(option)
+    return notify
+  },
+  warning: function (option) {
+    option.type = "warning"
+    let notify = new Notify(option)
+    return notify
   },
 }
 
